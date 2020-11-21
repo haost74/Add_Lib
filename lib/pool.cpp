b864219eda76e5387da035005a2ca0fea314dea4
haost <Aleksey.V.Gribkov@rt.ru>
2020-11-21 17:49:46 +0300

    first commit

---
+++ lib/pool.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "../include/static/pool.h"
+
+void Hello::print()
+{
+    std::cout << "Hello Static Library!" << " 10 " << std::endl;
+}
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include "../include/static/pool.h"
+
+int main(int argc, char *argv[])
+{
+    Hello hi;
+    hi.print();
+    return 0;
+}
